6e5fb411c8a041a0243ea87e1e1df30ab9f284be
vchivu14 <chivu.vlad@yahoo.com>
2022-03-05 15:40:44 +0100

    topics page SSR

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ app.use(express.static("public"));
 
 const fs = require("fs");
 
+const topicsPage = fs.readFileSync("./public/pages/topicspage/topicspage.html").toString();
+const topicsheader = fs.readFileSync("./public/pages/topicspage/components/header.html").toString();
+const topicspanelOne = fs.readFileSync("./public/pages/topicspage/components/panelOne.html").toString();
+const topicspanelTwo = fs.readFileSync("./public/pages/topicspage/components/panelTwo.html").toString();
+const topicspanelThree = fs.readFileSync("./public/pages/topicspage/components/panelThree.html").toString();
+const topicspanelFour = fs.readFileSync("./public/pages/topicspage/components/panelFour.html").toString();
+const topicspanelFive = fs.readFileSync("./public/pages/topicspage/components/panelFive.html").toString();
+const topicsfooter = fs.readFileSync("./public/pages/topicspage/components/footer.html").toString();
 
+const alltopicspage = topicsPage + topicsheader + topicspanelOne + topicspanelTwo + topicspanelThree + 
+                        topicspanelFour + topicspanelFive + topicsfooter;
+
+app.get("/", (req, res) => {
+    res.send(alltopicspage);
+})
 
 const PORT = process.env.PORT || 9000;
 const server = app.listen(PORT, () => {
